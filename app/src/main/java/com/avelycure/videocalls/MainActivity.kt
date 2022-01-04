bd67462a846b1c4b0bcbb156190df344970c378b
avelycure <van.emelin2012@yandex.ru>
2022-01-04 23:34:16 +0300

    Added ui for video screen

--- app/src/main/java/com/avelycure/videocalls/MainActivity.kt
+++ app/src/main/java/com/avelycure/videocalls/MainActivity.kt
@@ -3,22 +3,56 @@ package com.avelycure.videocalls
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgs
+import androidx.navigation.navArgument
 import com.avelycure.videocalls.ui.theme.VideoCallsTheme
 
+@ExperimentalUnsignedTypes
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VideoCallsTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Text(
-                        text = BuildConfig.APP_ID
-                    )
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "room_screen"
+                    ) {
+                        composable(route = "room_screen") {
+                            RoomScreen(onNavigate = navController::navigate)
+                        }
+                        composable(
+                            route = "video_screen/{roomName}",
+                            arguments = listOf(
+                                navArgument(name = "roomName") {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) {
+                            val roomName = it.arguments?.getString("roomName") ?: return@composable
+                            VideoScreen(
+                                roomName = roomName,
+                                onNavigateUp = navController::navigateUp
+                            )
+                        }
+
+                    }
                 }
             }
         }
